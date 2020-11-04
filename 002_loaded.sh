2208a5325857402f0235e0908373de75e9b532e3
Oleksandr Dievri <o.dievri@gmail.com>
2020-11-04 14:38:26 +0200

    add loaded example

---
+++ 002_loaded.sh
@@ -0,0 +1,19 @@
+#!/dis/sh.dis
+loaded
+
+### Output
+# ; ./002_loaded.sh
+# @	builtin
+# builtin	builtin
+# exit	builtin
+# load	builtin
+# loaded	builtin
+# run	builtin
+# syncenv	builtin
+# unload	builtin
+# whatis	builtin
+# ${bquote}	builtin
+# ${builtin}	builtin
+# ${loaded}	builtin
+# ${quote}	builtin
+# ${unquote}	builtin
