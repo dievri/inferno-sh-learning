979afc20a85ffcfd856f624772bba24b84680208
Oleksandr Dievri <o.dievri@gmail.com>
2020-11-04 16:43:28 +0200

    add expr example

---
+++ 010_expr.sh
@@ -0,0 +1,6 @@
+#!/dis/sh.dis
+load std
+load expr
+# you may use '*' instead of x for multiplication
+echo ${expr 5 2 3 + x}
+i = 10; while {ntest ${expr $i 5 -}} {echo -n $i^' '; i = ${expr $i 1 -}}; echo
